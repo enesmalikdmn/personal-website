8a46e4d419b1b68d2fe55bd92493e96ae720415d
enesmalikdmn <m.duman@digitastic.app>
2024-11-24 15:06:07 +0300

    feat: added new route

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function About() {
     return (
         <main className="flex text-primary bg-white h-full">
-           hdsgajhdga
+           About
         </main>
     )
 }
---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className="flex text-primary bg-white h-full">
+      Projects
+    </div>
+  )
+}
+
+export default Projects
