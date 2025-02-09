adb99346f927be60086e0b15cdefe3b74411a217
enesmalikdmn <m.duman@digitastic.app>
2025-02-09 21:47:16 +0300

    feat: experience and contact page header added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,17 @@
 export default function Contact() {
-    return (
-        <main className="flex text-primary bg-white h-full">
-           Contact
-        </main>
-    )
+  return (
+    <main className="flex flex-col text-primary bg-white h-full">
+      <div className="relative flex flex-col items-center m-32">
+        <div className="text-[8rem] uppercase font-semibold opacity-10 absolute top-1/2 -translate-y-1/2">
+          Contact
+        </div>
+        <div className="text-[2rem] font-semibold text-black relative z-10 border-b-2 border-[#BEBE28]">
+          Get In Touch
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-6 justify-center bg-white h-full">
+        This Page is Under Construction
+      </div>
+    </main>
+  );
 }
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,10 +1,17 @@
-
-
-
 export default function Experience() {
-    return (
-        <main className="flex text-primary bg-white h-full">
-           Experience
-        </main>
-    )
+  return (
+    <main className="flex flex-col text-primary bg-white h-full">
+      <div className="relative flex flex-col items-center m-32">
+        <div className="text-[8rem] uppercase font-semibold opacity-10 absolute top-1/2 -translate-y-1/2">
+          Experiences
+        </div>
+        <div className="text-[2rem] font-semibold text-black relative z-10 border-b-2 border-[#BEBE28]">
+          My Experience
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-6 justify-center bg-white h-full">
+        This Page is Under Construction
+      </div>
+    </main>
+  );
 }
